c95b2ec718d395b08e7b6d412de67e05b0da0235
huntermalm <huntrmalm@gmail.com>
2023-05-18 22:28:55 -0400

    Simplified inner-radius calculation

--- include/calculate.h
+++ include/calculate.h
@@ -19,23 +19,10 @@ namespace Layers
 
 	inline double inner_radius(int outer_radius, int thickness)
 	{
-		double border_lower_bound = 3;
-		double border_range = 27;
-		double border_percent = (thickness - border_lower_bound) / border_range;
+		double value = outer_radius - thickness;
 
-		double y_int_lower_bound = -2.2164;
-		double y_int_range = 2.5446;
-
-		double slope_lower_bound = 0.3435;
-		double slope_range = 0.6205;
-
-		double y_int = (border_percent * y_int_range) + y_int_lower_bound;
-
-		double slope = ((1 - border_percent) * slope_range) + slope_lower_bound;
-
-		double value = round(slope * double(outer_radius) + y_int);
-
-		if (value < 0) value = 0;
+		if (value < 0.0)
+			return 0.0;
 
 		return value;
 	}
