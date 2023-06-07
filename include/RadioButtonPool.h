f364cb838504980828892041894c2f93573c698f
huntermalm <huntrmalm@gmail.com>
2023-06-07 16:46:25 -0400

    Updated documentation for RadioButtonPool

--- include/RadioButtonPool.h
+++ include/RadioButtonPool.h
@@ -8,13 +8,24 @@ namespace Layers
 {
 	class RadioButton;
 
+	/*!
+		A RadioButtonPool is a QObject that ensures that within a group of
+		RadioButton widgets, only one is active at a time.
+	*/
 	class RadioButtonPool : public QObject
 	{
 		Q_OBJECT
 
 	public:
+		/*!
+			Constructs a radio button pool object with a set of
+			*radio_buttons*.
+		*/
 		RadioButtonPool(QList<RadioButton*> radio_buttons);
 
+		/*!
+			Sets *button* as the active radio button in the pool.
+		*/
 		void set_active(RadioButton* button);
 
 	private:
