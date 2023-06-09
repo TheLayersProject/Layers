a680f7fffd828b053bd5833dd2d77dc0befd53e4
huntermalm <huntrmalm@gmail.com>
2023-06-08 20:19:04 -0400

    Small changes to ThemeLineageModel

--- include/ThemeLineageModel.h
+++ include/ThemeLineageModel.h
@@ -20,16 +20,16 @@ namespace Layers
 
 		virtual int columnCount(const QModelIndex& index) const override;
 
-		virtual QVariant data(const QModelIndex& index, int role) const override;
+		virtual QVariant data(
+			const QModelIndex& index, int role) const override;
 
-		virtual QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+		virtual QVariant headerData(
+			int section, Qt::Orientation orientation, int role) const override;
 
 		virtual int rowCount(const QModelIndex& index) const override;
 
 	private:
 		QList<ThemeLineageData> m_lineage_data{ QList<ThemeLineageData>() };
-
-		//QList<Theme*> m_themes{ QList<Theme*>() };
 	};
 }
 
